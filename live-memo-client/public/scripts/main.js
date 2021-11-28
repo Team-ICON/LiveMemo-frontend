94ef9531cddd0de1b3830ec8acb2e2cfe67cf132
jungbum <jungbumwoo@naver.com>
2021-11-28 10:56:22 +0900

    ing

--- live-memo-client/public/scripts/main.js
+++ live-memo-client/public/scripts/main.js
@@ -1,63 +1,64 @@
 'use strict';
 
-const applicationServerPublicKey = `${process.env.REACT_APP_APPLICATION_SERVER_PUBLIC_KEY}`;
+// const applicationServerPublicKey = `${process.env.REACT_APP_APPLICATION_SERVER_PUBLIC_KEY}`;
 
-let isSubscribed = false;
+// let isSubscribed = false;
+// let swRegistration = null;
+
+// function urlB64ToUint8Array(base64String) {
+//   console.log(`base64String`, base64String);
+//   const padding = '='.repeat((4 - base64String.length % 4) % 4);
+//   const base64 = (base64String + padding)
+//     .replace(/\-/g, '+')
+//     .replace(/_/g, '/');
+
+//   const rawData = window.atob(base64);
+//   const outputArray = new Uint8Array(rawData.length);
+
+//   for (let i = 0; i < rawData.length; ++i) {
+//     outputArray[i] = rawData.charCodeAt(i);
+//   }
+//   console.log(`outputArray`, outputArray);
+//   return outputArray;
+// }
+
+
+// function subscribeUser() {
+//   const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+//   swRegistration.pushManager.subscribe({
+//     userVisibleOnly: true,
+//     applicationServerKey: applicationServerKey
+//   })
+//     .then(function (subscription) {
+//       console.log('User is subscribed.');
+//       console.log(`subscription`, subscription);
+
+//       isSubscribed = true;
+
+//     })
+//     .catch(function (err) {
+//       console.log('Failed to subscribe the user: ', err);
+//     });
+// }
+
+
+// function initializeUI() {
+//   // Set the initial subscription value
+//   swRegistration.pushManager.getSubscription()
+//     .then(function (subscription) {
+//       isSubscribed = !(subscription === null);
+
+//       if (isSubscribed) {
+//         console.log('User IS subscribed.');
+//         console.log(`subscription`, subscription);
+//       } else {
+//         console.log('User is NOT subscribed.');
+//         subscribeUser();
+//       }
+
+//     });
+// }
 let swRegistration = null;
-
-function urlB64ToUint8Array(base64String) {
-  console.log(`base64String`, base64String);
-  const padding = '='.repeat((4 - base64String.length % 4) % 4);
-  const base64 = (base64String + padding)
-    .replace(/\-/g, '+')
-    .replace(/_/g, '/');
-
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  console.log(`outputArray`, outputArray);
-  return outputArray;
-}
-
-
-function subscribeUser() {
-  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
-  swRegistration.pushManager.subscribe({
-    userVisibleOnly: true,
-    applicationServerKey: applicationServerKey
-  })
-    .then(function (subscription) {
-      console.log('User is subscribed.');
-      console.log(`subscription`, subscription);
-
-      isSubscribed = true;
-
-    })
-    .catch(function (err) {
-      console.log('Failed to subscribe the user: ', err);
-    });
-}
-
-
-function initializeUI() {
-  // Set the initial subscription value
-  swRegistration.pushManager.getSubscription()
-    .then(function (subscription) {
-      isSubscribed = !(subscription === null);
-
-      if (isSubscribed) {
-        console.log('User IS subscribed.');
-        console.log(`subscription`, subscription);
-      } else {
-        console.log('User is NOT subscribed.');
-        subscribeUser();
-      }
-
-    });
-}
 
 if ('serviceWorker' in navigator && 'PushManager' in window) {
   console.log('Service Worker and Push is supported');
@@ -67,7 +68,7 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
       console.log('Service Worker is registered', swReg);
 
       swRegistration = swReg;
-      initializeUI();
+      // initializeUI();
     })
     .catch(function (error) {
       console.error('Service Worker Error', error);
