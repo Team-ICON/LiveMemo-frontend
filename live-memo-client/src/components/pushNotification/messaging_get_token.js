dd3cf6549c0a56ed0bae3782876c1d039d8023fb
KsPsD <ui9621@naver.com>
2021-11-29 15:47:38 +0900

    service worker fix

--- live-memo-client/src/components/pushNotification/messaging_get_token.js
+++ live-memo-client/src/components/pushNotification/messaging_get_token.js
@@ -1,51 +1,51 @@
-// import { getMessaging, getToken, onMessage } from "firebase/messaging";
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+// // import { getMessaging, getToken, onMessage } from "firebase/messaging";
+// // // Import the functions you need from the SDKs you need
+// // import { initializeApp } from "firebase/app";
+// // import { getAnalytics } from "firebase/analytics";
+// // import { api } from "../../axios";
+// // // import { onBackgroundMessage } from "firebase/messaging/sw";
+// // import dotenv from "dotenv";
+
+// // dotenv.config();
+
+
+// import { messaging, getToken, onMessage } from "../../firebase"
 // import { api } from "../../axios";
-// // import { onBackgroundMessage } from "firebase/messaging/sw";
-// import dotenv from "dotenv";
 
-// dotenv.config();
+// function subscribe() {
+//   Notification.requestPermission().then(permission => {
+//     console.log(permission)
+//     if (permission == "granted") {
+//       getToken(messaging, { vapidKey: `BEqPyH6fNMq7qKa5Sn81R3VeI5Nw0kjR3gea79SkfJpzEocFro5ljOubelpLcn7QX7JiQAksBWT1VudTQklyfWQ` }).then(async (currentToken) => {
+//         if (currentToken) {
+//           // Send the token to your server and update the UI if necessary
+//           console.log(`currentToken`, currentToken);
 
+//           api.put('user/userFcmToken', {
+//             fcmToken: currentToken
+//           }).then((result) => {
+//             if (result) {
+//               console.log(`result`, result);
+//               console.log('save fcm token');
+//             }
+//           }).catch((err) => {
+//             console.log(`err at getToken(fcm)`, err);
+//           })
 
-import { messaging, getToken, onMessage } from "../../firebase"
-import { api } from "../../axios";
+//         } else {
+//           // Show permission request UI
+//           console.log('No registration token available. Request permission to generate one.');
+//         }
+//       }).catch((err) => {
+//         console.log('An error occurred while retrieving token. ', err);
+//       });
 
-function subscribe() {
-  Notification.requestPermission().then(permission => {
-    console.log(permission)
-    if (permission == "granted") {
-      getToken(messaging, { vapidKey: `BEqPyH6fNMq7qKa5Sn81R3VeI5Nw0kjR3gea79SkfJpzEocFro5ljOubelpLcn7QX7JiQAksBWT1VudTQklyfWQ` }).then(async (currentToken) => {
-        if (currentToken) {
-          // Send the token to your server and update the UI if necessary
-          console.log(`currentToken`, currentToken);
+//     }
 
-          api.put('user/userFcmToken', {
-            fcmToken: currentToken
-          }).then((result) => {
-            if (result) {
-              console.log(`result`, result);
-              console.log('save fcm token');
-            }
-          }).catch((err) => {
-            console.log(`err at getToken(fcm)`, err);
-          })
+//   }).catch(e => {
+//     console.log(e)
+//   })
+// }
 
-        } else {
-          // Show permission request UI
-          console.log('No registration token available. Request permission to generate one.');
-        }
-      }).catch((err) => {
-        console.log('An error occurred while retrieving token. ', err);
-      });
-
-    }
-
-  }).catch(e => {
-    console.log(e)
-  })
-}
-
-subscribe();
+// subscribe();
 
