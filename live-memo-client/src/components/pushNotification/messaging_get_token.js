ac8ab14b3e7be870659d0eaab612e2a4996f1e42
KsPsD <ui9621@naver.com>
2021-11-29 15:46:59 +0900

    service worker fix

--- live-memo-client/src/components/pushNotification/messaging_get_token.js
+++ live-memo-client/src/components/pushNotification/messaging_get_token.js
@@ -12,40 +12,40 @@
 import { messaging, getToken, onMessage } from "../../firebase"
 import { api } from "../../axios";
 
-function subscribe() {
-  Notification.requestPermission().then(permission => {
-    console.log(permission)
-    if (permission == "granted") {
-      getToken(messaging, { vapidKey: `BEqPyH6fNMq7qKa5Sn81R3VeI5Nw0kjR3gea79SkfJpzEocFro5ljOubelpLcn7QX7JiQAksBWT1VudTQklyfWQ` }).then(async (currentToken) => {
-        if (currentToken) {
-          // Send the token to your server and update the UI if necessary
-          console.log(`currentToken`, currentToken);
+// function subscribe() {
+//   Notification.requestPermission().then(permission => {
+//     console.log(permission)
+//     if (permission == "granted") {
+//       getToken(messaging, { vapidKey: `BEqPyH6fNMq7qKa5Sn81R3VeI5Nw0kjR3gea79SkfJpzEocFro5ljOubelpLcn7QX7JiQAksBWT1VudTQklyfWQ` }).then(async (currentToken) => {
+//         if (currentToken) {
+//           // Send the token to your server and update the UI if necessary
+//           console.log(`currentToken`, currentToken);
 
-          api.put('user/userFcmToken', {
-            fcmToken: currentToken
-          }).then((result) => {
-            if (result) {
-              console.log(`result`, result);
-              console.log('save fcm token');
-            }
-          }).catch((err) => {
-            console.log(`err at getToken(fcm)`, err);
-          })
+//           api.put('user/userFcmToken', {
+//             fcmToken: currentToken
+//           }).then((result) => {
+//             if (result) {
+//               console.log(`result`, result);
+//               console.log('save fcm token');
+//             }
+//           }).catch((err) => {
+//             console.log(`err at getToken(fcm)`, err);
+//           })
 
-        } else {
-          // Show permission request UI
-          console.log('No registration token available. Request permission to generate one.');
-        }
-      }).catch((err) => {
-        console.log('An error occurred while retrieving token. ', err);
-      });
+//         } else {
+//           // Show permission request UI
+//           console.log('No registration token available. Request permission to generate one.');
+//         }
+//       }).catch((err) => {
+//         console.log('An error occurred while retrieving token. ', err);
+//       });
 
-    }
+//     }
 
-  }).catch(e => {
-    console.log(e)
-  })
-}
+//   }).catch(e => {
+//     console.log(e)
+//   })
+// }
 
-subscribe();
+// subscribe();
 
--- live-memo-client/src/firebase.js
+++ live-memo-client/src/firebase.js
@@ -2,6 +2,8 @@ import { getMessaging, getToken, onMessage, isSupported } from "firebase/messagi
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { api } from "./axios";
+
 // import { onBackgroundMessage } from "firebase/messaging/sw";
 import dotenv from "dotenv";
 
@@ -19,5 +21,42 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const messaging = isSupported() ? getMessaging() : null
 
+function subscribe() {
+    Notification.requestPermission().then(permission => {
+        console.log(permission)
+        if (permission == "granted") {
+            getToken(messaging, { vapidKey: `BEqPyH6fNMq7qKa5Sn81R3VeI5Nw0kjR3gea79SkfJpzEocFro5ljOubelpLcn7QX7JiQAksBWT1VudTQklyfWQ` }).then(async (currentToken) => {
+                if (currentToken) {
+                    // Send the token to your server and update the UI if necessary
+                    console.log(`currentToken`, currentToken);
+
+                    api.put('user/userFcmToken', {
+                        fcmToken: currentToken
+                    }).then((result) => {
+                        if (result) {
+                            console.log(`result`, result);
+                            console.log('save fcm token');
+                        }
+                    }).catch((err) => {
+                        console.log(`err at getToken(fcm)`, err);
+                    })
+
+                } else {
+                    // Show permission request UI
+                    console.log('No registration token available. Request permission to generate one.');
+                }
+            }).catch((err) => {
+                console.log('An error occurred while retrieving token. ', err);
+            });
+
+        }
+
+    }).catch(e => {
+        console.log(e)
+    })
+}
+
+if (messaging)
+    subscribe();
 
 export { messaging, getToken, onMessage, app }
