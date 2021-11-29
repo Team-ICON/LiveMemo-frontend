a2cf1bcb41b696c83692ba9d65695d85dbdfd1a9
AnSolomon <solomonivy@naver.com>
2021-11-29 09:48:19 +0900

    img name change

--- live-memo-client/public/firebase-messaging-sw.js
+++ live-memo-client/public/firebase-messaging-sw.js
@@ -41,7 +41,7 @@ self.addEventListener('push', function (event) {
     const title = jsonText.notification.title;
     const options = {
         body: jsonText.notification.body,
-        icon: 'icons/Live-memo(192x192).png',
+        icon: 'icons/Live-Memo(192x192).png',
         badge: 'images/badge.png'
     };
     event.waitUntil(self.registration.showNotification(title, options));
