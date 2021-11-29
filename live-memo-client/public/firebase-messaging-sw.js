a515c68936fa356447839e04c97b5395546188fd
KsPsD <ui9621@naver.com>
2021-11-29 12:48:26 +0900

    change config

--- live-memo-client/public/firebase-messaging-sw.js
+++ live-memo-client/public/firebase-messaging-sw.js
@@ -1,8 +1,33 @@
-
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging/sw";
+import { messaging, getToken, onMessage } from "../src/firebase"
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
+const firebaseApp = initializeApp({
 
+
+    apiKey: "AIzaSyC2XKZyh4QMPROuZXDneko-9X9MoGde-Lc",
+    authDomain: "live-memo-610d4.firebaseapp.com",
+    projectId: "live-memo-610d4",
+    storageBucket: "live-memo-610d4.appspot.com",
+    messagingSenderId: "874159596175",
+    appId: "1:874159596175:web:f2a1236ba6e24f5a9286fa"
+
+
+});
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+// 이 아래 부분을 설정하는 곳이 종종 보이는데 이걸 하면 같은 알림이 2개 온다.
+// messaging.onBackgroundMessage(function (payload) {
+//     console.log('[firebase-messaging-sw.js] onBackgroundMessage ', payload)
+//     const title = payload.notification.title
+//     const options = {
+//         body: payload.notification.body,
+//         icon: payload.notification.image
+//     }
+//     return self.registration.showNotification(title, options)
+// })
 
 self.addEventListener('push', function (event) {
     console.log('event1111111111111111111111111111111111', event);
@@ -34,5 +59,5 @@ self.addEventListener('notificationclick', function (event) {
 
     //   var data = event.notification.data;
     event.notification.close();
-    event.waitUntil(clients.openWindow("https://live-memo-610d4.firebaseapp.com/"));
+    event.waitUntil(clients.openWindow("https://livememo-frontend.web.app/"));
 });
