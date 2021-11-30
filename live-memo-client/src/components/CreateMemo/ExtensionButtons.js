5324de043fd89baa08ba54ef875f0dd9ca9bd7a8
KsPsD <ui9621@naver.com>
2021-11-30 13:28:09 +0900

    add button in editor

---
+++ live-memo-client/src/components/CreateMemo/ExtensionButtons.js
@@ -0,0 +1,43 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import { useCommands, useActive } from '@remirror/react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { cx } from 'remirror';
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            // Purple and green play nicely together.
+            main: "#fff"
+        },
+    },
+});
+const ExtensionButtons = () => {
+    const commands = useCommands();
+    const active = useActive(true);
+    return (
+        <ThemeProvider theme={theme}>
+
+            <Stack className="buttonGroup" direction="row" spacing={2}>
+
+                <Button variant="outlined" onMouseDown={(event) => event.preventDefault()}
+                    onClick={() => commands.setTextHighlight('yellow')}>Hilight</Button>
+                <Button variant="outlined" onMouseDown={(event) => event.preventDefault()}
+                    onClick={() => commands.removeTextHighlight()}>remove</Button>
+                <Button variant="outlined" onMouseDown={(event) => event.preventDefault()}
+                    onClick={() => commands.toggleBold()}
+                    className={cx(active.bold() && 'active')}
+                >
+                    Bold</Button>
+
+            </Stack >
+
+        </ThemeProvider>
+
+
+    );
+};
+
+export default ExtensionButtons
