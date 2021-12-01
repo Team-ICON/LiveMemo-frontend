c93745062f9b7daad9168a126be748052d592a28
KsPsD <ui9621@naver.com>
2021-12-01 22:26:15 +0900

    change to icon

--- live-memo-client/src/components/CreateMemo/ExtensionButtons.js
+++ live-memo-client/src/components/CreateMemo/ExtensionButtons.js
@@ -4,7 +4,10 @@ import { useCommands, useActive } from '@remirror/react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import { cx } from 'remirror';
-
+import AutoFixNormalIcon from '@mui/icons-material/AutoFixNormal';
+import AutoFixOffIcon from '@mui/icons-material/AutoFixOff';
+import FormatBoldIcon from '@mui/icons-material/FormatBold';
+import { IconButton } from '@mui/material';
 
 const theme = createTheme({
     palette: {
@@ -22,16 +25,19 @@ const ExtensionButtons = () => {
         <ThemeProvider theme={theme}>
 
             <Stack className="buttonGroup" direction="row" spacing={2}>
-
-                <Button variant="outlined" onMouseDown={(event) => event.preventDefault()}
-                    onClick={() => commands.setTextHighlight('yellow')}>Hilight</Button>
-                <Button variant="outlined" onMouseDown={(event) => event.preventDefault()}
-                    onClick={() => commands.removeTextHighlight()}>remove</Button>
-                <Button variant="outlined" onMouseDown={(event) => event.preventDefault()}
+                <IconButton variant="outlined" onMouseDown={(event) => event.preventDefault()}
+                    onClick={() => commands.setTextHighlight('yellow')}>
+                    <AutoFixNormalIcon style={{ color: 'white' }} />
+                </IconButton>
+                <IconButton variant="outlined" onMouseDown={(event) => event.preventDefault()}
+                    onClick={() => commands.removeTextHighlight()}>
+                    <AutoFixOffIcon style={{ color: 'white' }} />
+                </IconButton>
+                <IconButton variant="outlined" onMouseDown={(event) => event.preventDefault()}
                     onClick={() => commands.toggleBold()}
-                    className={cx(active.bold() && 'active')}
-                >
-                    Bold</Button>
+                    className={cx(active.bold() && 'active')}>
+                    <FormatBoldIcon style={{ color: 'white' }} />
+                </IconButton>
 
 
             </Stack >
