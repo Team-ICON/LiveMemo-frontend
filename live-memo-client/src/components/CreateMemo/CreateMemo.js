e90abd2bb880c782e0ec46497d347ea5ff03f1b6
KsPsD <ui9621@naver.com>
2021-12-02 20:48:32 +0900

    fix bug

--- live-memo-client/src/components/CreateMemo/CreateMemo.js
+++ live-memo-client/src/components/CreateMemo/CreateMemo.js
@@ -147,10 +147,11 @@ function CreateMemo({ currentUser, socket }) {
                 setMemberList(res.data.memInfo.userList);
 
                 if (curMem.length == 1)
-                    return res.data.memInfo.content;
+                    return { data: res.data.memInfo.content, flag: true }
                 else {
-                    return "false"
+                    return { data: firstState, flag: false }
                 }
+
 
             }
         } catch {
