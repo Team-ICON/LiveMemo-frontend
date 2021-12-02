34e6a467d75838d463c939a370a4835c4316642a
KsPsD <ui9621@naver.com>
2021-12-02 21:04:29 +0900

    check

--- live-memo-client/src/components/editor/Editor.tsx
+++ live-memo-client/src/components/editor/Editor.tsx
@@ -161,15 +161,15 @@ function Editor({ documentId, onFetch, onSave, }: EditorProps) {
                 const res = await onFetch(documentId);
                 // res는 문자열이여서 여기서 JSON형태로 넘겨줘야됨 위에서 JSON으로 받아서 통일시킴 그래서 나중에 create에서 stringify 함
                 dispatch(selectDoc({
-                    docState: JSON.parse(res)
+                    docState: JSON.parse(res.data)
                 }))
                 if (res.flag) {
-                    console.log(res)
-                    getContext()?.setContent(JSON.parse(res));
+                    console.log(res.data)
+                    getContext()?.setContent(JSON.parse(res.data));
 
                 }
                 else {
-                    console.log(res)
+                    console.log(res.data)
                 }
 
             }
