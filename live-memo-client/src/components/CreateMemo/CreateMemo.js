145c248d2b00350c9748fdc1851b0ebf9bb69c27
KsPsD <ui9621@naver.com>
2021-12-06 14:39:06 +0900

    change font

--- live-memo-client/src/components/CreateMemo/CreateMemo.js
+++ live-memo-client/src/components/CreateMemo/CreateMemo.js
@@ -498,6 +498,7 @@ function CreateMemo({ currentUser, socket }) {
                                 <SearchIcon />
                             </SearchIconWrapper>
                             <StyledInputBase
+
                                 placeholder="사용자 메일을 입력해주세요."
                                 inputProps={{ 'aria-label': 'search' }}
                                 onChange={(e) => handleChange(e)}
